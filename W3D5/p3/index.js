d7804091ae2d2eb7bc3d1af9c758ef24e0fd301e
Salmdo <118146780+Salmdo@users.noreply.github.com>
2023-02-02 17:15:57 -0600

    update

---
+++ W3D5/p3/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const date = new Date();
+const hour = date.getHours();
+
+app.use(express.static(path.join(__dirname, '/css')))
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'/index.html'));
+})
+
+app.post('/result',(req,res) => {
+    res.send(`Welcome ${req.body.name}, your age is ${req.body.age}`);
+})
+
+app.listen(3001, () => { console.log("Server q3 is running"); })
